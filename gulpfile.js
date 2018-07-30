7b186d3b854da36f01912088937347d8dbc22a40
CatK1t <polovnikov1491@gmail.com>
2018-07-30 12:20:57 +0300

    Less Shablon

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp          = require('gulp');
+const cleanCSS      = require('gulp-clean-css');
+const autoprefixer  = require('gulp-autoprefixer');
+const browsersync   = require('browser-sync');
+const sourcemaps    = require('gulp-sourcemaps');
+const concat        = require('gulp-concat');
+const uglify        = require('gulp-uglify');
+const gcmq          = require('gulp-group-css-media-queries');
+const preproc        = require('gulp-less');
+
+let config = {
+    src : './src',
+    css: {
+        watch: '/precss/**/*.less',
+        src: '/precss/+(style|style-IE9.less)',
+        dest: '/css'
+    },
+    preJs: {
+        libs: '/libs/**/*.js',
+        js: '/js',
+        jsf: '/js/main.js'
+    },
+    html: {
+        src: '/*.html'
+    }
+};
+
+gulp.task('build', function(){
+    gulp.src(config.src + config.css.src)
+    .pipe(sourcemaps.init())
+    .pipe(preproc())
+    .pipe(gcmq())
+    .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+    }))
+    .pipe(cleanCSS(
+       {level: 2}
+    ))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.src + config.css.dest))
+    .pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('browser-sync', function() {
+    browsersync({
+    server: {
+        baseDir: config.src
+    },
+    notify: false,
+    // open: false,
+    // tunnel: true,
+    // tunnel: "projectname", 
+    //Demonstration page: 		http://projectname.localtunnel.me
+    })
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'src/libs/jquery/dist/jquery.min.js',
+		config.src +  config.preJs.jsf, // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest(config.src +  config.preJs.js))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('watch', ['build', 'js','browser-sync'], function(){
+    gulp.watch(config.src + config.css.watch,['build']);
+    gulp.watch([config.src + config.preJs.libs, config.src +  config.preJs.jsf], ['js']);
+    gulp.watch(config.src + config.html.src, browsersync.reload);
+});
+
+gulp.task('default', ['watch']);
---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+// Функция для кроссбраузерности (для браузеров которые не поддерживают calc())
+
+window.onload = function(){
+    
+    if(!checkCalc()){
+        var style = document.createElement('link');
+        style.setAttribute('rel','stylesheet');
+        style.setAttribute('href','css/style-IE9.css');
+        document.body.appendChild(style);
+    }
+
+}
+
+function checkCalc(){
+    var div = document.createElement('div');
+    div.style.cssText = 'width: calc(100%)';
+    return div.style.length > 0;
+}
+
+// Конец проверки
+
+// Код страницы Старт
